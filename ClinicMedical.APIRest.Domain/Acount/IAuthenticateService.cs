16d60234578917fce1ed50533c537e0bc5fe6d7d
georghiton-luiz <georghiton@gmail.com>
2023-12-12 14:00:45 -0300

    Implementação da interface de autenticação

---
+++ ClinicMedical.APIRest.Domain/Acount/IAuthenticateService.cs
@@ -0,0 +1,18 @@
+﻿using ClinicMedical.APIRest.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicMedical.APIRest.Domain.Acount
+{
+    public interface IAuthenticateService
+    {
+        Task<bool>AuthenticateAsync(string login, string password);
+        Task<bool> UserExistsByCPF_CNPJ(string cPF_CNPJ);
+        Task<bool> IsLoginExists(string cPF_CNPJ);
+        Task<PatientEntity> UserExistsByLogin(string login);
+        public string GenerationToken(int id, string login);
+    }
+}
